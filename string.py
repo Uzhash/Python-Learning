d80d7b598d7f2e423c3912c8fc392df8e64ea515
Uzhash <muzaibakhtar5@gmail.com>
2026-02-28 23:20:16 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,18 @@
+name="Uzaib"
+friend="Ali"
+anotherFriend='Ahmed'
+
+name="Uzaib"
+print("Hi, "+ name)
+
+st='''  Python is one of the most demanded programming languages in the job market. Surprisingly, it is equally easy to learn and master  Python. This python tutorial for absolute beginners in Hindi series will focus on teaching you python concepts from the ground up. '''
+print(st)
+
+print(name[0])
+print(name[1])
+print(name[2])  
+print(name[3])
+print(name[4])
+print("Let's use a for loop\n")
+for character in st:
+    print(character)
