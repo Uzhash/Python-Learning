930324cbbb6ab4d40416e98f797b977f9afb3d5a
Uzhash <muzaibakhtar5@gmail.com>
2026-03-04 22:26:04 +0500

    Add files via upload

---
+++ string slicing.py
@@ -0,0 +1,14 @@
+# names="Uzaib,Ali"
+# print(names[6:9],names[0:5])
+# print(len(names))
+fruit="Mango"
+mangoLen=len(fruit)
+print(mangoLen)
+# print(fruit[0:4])
+# print(fruit[1:4])
+# print(fruit[:])
+# print(fruit[:-3])
+# print(fruit[:len(fruit)-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
+# print("Apple is a",len1,"letter word")
